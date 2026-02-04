5721e5849413c00d9d5dccd3851a81cd9269d1b0
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-02-04 21:04:51 +0200

    feat(router): add more routes
    - add routes for Products, Categories, Cart, Favorites, and Shop.
    - add /home path to render Home alongside / path.

--- src/Routes/App_routes/index.jsx
+++ src/Routes/App_routes/index.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "../../Pages/Home";
 import Layout from "../../Components/Layout/Layout";
+import Home from "../../Pages/Home";
+import Cart from "../../Pages/Cart";
+import Favorites from "../../Pages/Favourit";
+import Shop from "../../Pages/Shop";
 import Contact from "../../Pages/ContactUs";
 import About from "../../Pages/About";
 import LoginPage from "../../Pages/SignIn";
 import SignUp from "../../Pages/SignUp";
 import NotFound from "../../Pages/NotFound";
+import Categories from "../../Pages/Categories";
+import Products from "../../Pages/Products";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +20,30 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />
+            },
+            {
+                path: "/home",
+                element: <Home />
+            },
+            {
+                path: "/categories",
+                element: <Categories />
+            },
+            {
+                path: "/products",
+                element: <Products />
+            },
+            {
+                path: "/cart",
+                element: <Cart />
+            },
+            {
+                path: "/favorites",
+                element: <Favorites />
+            },
+            {
+                path: "/shop",
+                element: <Shop />
             },
             {
                 path: "/contact",
